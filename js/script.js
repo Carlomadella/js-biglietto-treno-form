8ba9589afc9a69c29bdb96f4e12200ce8321ef52
Carlomadella <madella871@gmail.com>
2025-03-27 00:36:29 +0100

    create le variabili utili da riprendere in seguito e stampare sulla card

--- js/script.js
+++ js/script.js
@@ -7,34 +7,6 @@ const button = document.getElementById("button");
 const distanceOutput = document.getElementById("distanceOutput");
 const ageOutput = document.getElementById("ageOutput");
 const nameOutput = document.getElementById("nameOutput");
-const priceOutput = document.getElementById("priceOutput");
-
-form.addEventListener("submit", function(event) {
-    
-    event.preventDefault();
-
-    const distanzaTot = document.getElementById("distanzaInput").value;
-    const passengerAge = document.getElementById("etàInput").value;
-    const nominativo = document.getElementById("nominativoInput").value;
-    let priceKm = 0.21;
-
-    let prezzoIntero = (parseInt(priceKm * distanzaTot));
-    let prezzoTotale = prezzoIntero;
-
-    if (passengerAge<18) {
-        prezzoTotale = (prezzoIntero-( 20/100 ))
-    } 
-    else if (passengerAge>65){
-        prezzoTotale = (prezzoIntero-( 40/100 ))
-    }
-    else{
-        prezzoTotale = prezzoIntero
-    }
-
-    nameOutput.innerHTML = nominativo;
-    distanceOutput.innerHTML = distanzaTot;
-    ageOutput.innerHTML = passengerAge;
-    priceOutput.innerHTML = prezzoTotale;
-});
+const priceOutput = document.getElementById("priceOutput")
 
 
