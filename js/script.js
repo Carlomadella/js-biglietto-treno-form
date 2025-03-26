bbd19e4f5b800afedcf7e61025e4ddbba3854a37
Carlomadella <madella871@gmail.com>
2025-03-27 00:29:50 +0100

    completato js ora manca solo il css

--- js/script.js
+++ js/script.js
@@ -1,26 +1,40 @@
-let journeyLenght = parseInt(prompt("Indica i kilometri che vuoi percorrere con questo treno?"));
-let passengerAge = parseInt(prompt("Indica l'età del passengero"));
-let priceKm = 0.21;
+// MILESTONE 2: Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo. Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
-// console.log(journeyLenght)
-// console.log(passengerAge)
+// creo le variabili utili 
+const form = document.querySelector("form");
+const button = document.getElementById("button");
 
-let prezzoIntero = (parseInt(priceKm * journeyLenght));
-let prezzoTotale = prezzoIntero;
-// console.log("Il prezzo finale è di", prezzoTotale, "€");
+const distanceOutput = document.getElementById("distanceOutput");
+const ageOutput = document.getElementById("ageOutput");
+const nameOutput = document.getElementById("nameOutput");
+const priceOutput = document.getElementById("priceOutput");
 
-if (passengerAge<18) {
-    prezzoTotale = (prezzoIntero-( 20/100 ))
-    // console.log("Il prezzo del biglietto è scontato del 20%, perciò il totale sarà di", prezzoTotale, "€")
-} 
-else if (passengerAge>65){
-    prezzoTotale = (prezzoIntero-( 40/100 ))
-    // console.log("Il prezzo del biglietto è scontato del 40%, perciò il totale sarà di", prezzoTotale, "€")
-}
-else{
-    prezzoTotale = prezzoIntero
-}
+form.addEventListener("submit", function(event) {
+    
+    event.preventDefault();
 
-prezzoTotale = (prezzoTotale).toFixed(2); 
+    const distanzaTot = document.getElementById("distanzaInput").value;
+    const passengerAge = document.getElementById("etàInput").value;
+    const nominativo = document.getElementById("nominativoInput").value;
+    let priceKm = 0.21;
 
-console.log("Il prezzo totale da pagare è di", prezzoTotale, "€")
+    let prezzoIntero = (parseInt(priceKm * distanzaTot));
+    let prezzoTotale = prezzoIntero;
+
+    if (passengerAge<18) {
+        prezzoTotale = (prezzoIntero-( 20/100 ))
+    } 
+    else if (passengerAge>65){
+        prezzoTotale = (prezzoIntero-( 40/100 ))
+    }
+    else{
+        prezzoTotale = prezzoIntero
+    }
+
+    nameOutput.innerHTML = nominativo;
+    distanceOutput.innerHTML = distanzaTot;
+    ageOutput.innerHTML = passengerAge;
+    priceOutput.innerHTML = prezzoTotale;
+});
+
+
